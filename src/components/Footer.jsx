5243125c8a87f6900aaf37b852b5f03cdf37574c
onyangoouma <jacksonouma04@gmail.com>
2022-07-28 15:24:30 +0300

    ecommerce

---
+++ src/components/Footer.jsx
@@ -0,0 +1,58 @@
+import { faFacebook, faInstagramSquare, faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                   
+                    <div className="content">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque saepe sunt sed eius reiciendis similique ullam animi in, quasi ipsum maiores, necessitatibus dignissimos id quaerat facere architecto at, molestias harum!</p>
+                    </div>
+                </div>
+                <div className="col">
+                    <h3>Quick Links</h3>
+                    <div className="nav">
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/products">Products</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">About</Link>
+                            </li>
+                            <li>
+                                <Link to="/contact">Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="col">
+                    <h3>Follow Us</h3>
+                    <div className="right">
+                        <Link to="/"><FontAwesomeIcon icon={faFacebook} /></Link>
+                        <Link to="/"><FontAwesomeIcon icon={faInstagramSquare} /></Link>
+                        <Link to="/"><FontAwesomeIcon icon={faTwitterSquare} /></Link>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <hr />
+                    <div className="copyRights">
+                        <p className="copy">&copy; 2022. All Rights Reserved. Powered by Miljan Peric. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
