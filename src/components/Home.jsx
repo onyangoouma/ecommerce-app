e684a620cd2003d282a98d9aa918fd0d29e47320
onyangoouma <jacksonouma04@gmail.com>
2022-07-29 07:27:28 +0300

    added properties in the function created

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,26 @@ import Products from './Products'
 
 const Home = () => {
   return (
-   
+    <div className='home'>
+        <div className="hero">
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <img src="./assets/images/hero.jpg" alt="Hero" />
+                        <div className="col"> 
+                        <img src="./assets/images/hero5.jpg" alt="Hero5" />
+                        <div className="content">
+                            <h5 className="title">Cash and Carry!! Arrivals</h5>
+                            <p className="subtitle">We have latest Fashions in our Store</p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <Products />
+    </div>
+    
   )
 }
 
