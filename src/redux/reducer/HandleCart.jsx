4cec465cf01cfb90a07ce9ac0ac126480406f739
onyangoouma <jacksonouma04@gmail.com>
2022-07-29 10:20:15 +0300

    added case statement in HandleCart

--- src/redux/reducer/HandleCart.jsx
+++ src/redux/reducer/HandleCart.jsx
@@ -4,22 +4,7 @@ const HandleCart = (state = cart, action) => {
     
     const product = action.payload;
 
-    switch (action.type) {
-        case "ADDITEM":
-            //If product already exist
-            const exist = state.find((x) => x.id === product.id);
-            if(exist) {
-                //increase for quantity if already exist product in cart
-                return state.map((x) => x.id === product.id ? {...x, qty: x.qty + 1} : x);
-            } else {
-                //for new product in cart
-                const product = action.payload;
-                return [
-                    ...state,
-                    {
-                        ...product,
-                        qty: 1,
-                    }
+    
                 ]
             }
             case "DELETEITEM":
